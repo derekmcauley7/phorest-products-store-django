328bb1c74a47f06fb29a7ffd7bd3d8909410a87c
derekmcauley <derekmcauley7@gmail.com>
2022-09-25 16:33:15 +0100

    fixing shopping cart tests

--- shoppingcart/tests/test_views.py
+++ shoppingcart/tests/test_views.py
@@ -1,6 +1,7 @@
 from email.mime import image
 from urllib import response
 from django.test import TestCase, Client
+from django.contrib.auth.models import User
 from django.urls import reverse
 from product.models import Product
 
@@ -15,17 +16,27 @@ class TestViews(TestCase):
             quantityInStock = 1,
             image = 'http://someurl/someimage.png',
         )
+        self.credentials = {
+            'username': 'testuser',
+            'password': 'secret'}
+        User.objects.create_user(**self.credentials)
 
 
     def test_add_cart_item_view(self):
+        # login user 
+        self.client.post('/login', self.credentials, follow=True)
+
         response = self.client.get(reverse('cart_add', args=[1]))
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.status_code, 302)
 
     def test_clear_cart_should_redirect_view(self):
         response = self.client.get(reverse('item_clear', args=[1]))
         self.assertEqual(response.status_code, 302)
 
     def test_cart_details_view(self):
+        # login user 
+        self.client.post('/login', self.credentials, follow=True)
+
         response = self.client.get(reverse('cart_detail'))
         self.assertTemplateUsed('display-cart.html')
         self.assertEqual(response.status_code, 200)
