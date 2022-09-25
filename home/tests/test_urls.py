bf0eec8df465f67f32d5d865877865fd76547a3c
derekmcauley <derekmcauley7@gmail.com>
2022-09-25 14:04:57 +0100

    first commit

---
+++ home/tests/test_urls.py
@@ -0,0 +1,9 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from home.views import HomeView
+
+class TestUrls(SimpleTestCase):
+
+    def test_home_url(self):
+        url = reverse('home')
+        self.assertEqual(resolve(url).func.view_class, HomeView)
