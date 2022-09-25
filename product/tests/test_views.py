17e89d99565f0df40e92b1695e6c7a3af2d4cc67
derekmcauley <derekmcauley7@gmail.com>
2022-09-25 17:04:04 +0100

    mocking product for test

--- product/tests/test_views.py
+++ product/tests/test_views.py
@@ -1,8 +1,18 @@
 from django.test import TestCase, Client
 from django.urls import reverse
+from product.models import Product
 
 
 class TestViews(TestCase):
+
+    def setUp(self):
+        product = Product.objects.create(
+            id = 1,
+            name="test product",
+            price = 20,
+            quantityInStock = 1,
+            image = 'http://someurl/someimage.png',
+        )
 
     def test_product_list(self):
         client = Client()
