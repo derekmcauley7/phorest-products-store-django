d2965b900afc67298236d3f21f83f6567c2a1904
derekmcauley <derekmcauley7@gmail.com>
2022-10-06 09:59:12 +0100

    Small refactoring of test

--- phorestapi/tests/test_api_json.py
+++ phorestapi/tests/test_api_json.py
@@ -17,12 +17,12 @@ class TestAPI(TestCase):
         self.order_item2 = OrderItem.objects.create(order = self.order, price = 20, quantity =1, product = self.product)
 
     def test_product_items_json(self):
-        expected_json = [{"branchProductId": "1", "price": 10.00, "quantity": 1}, {"branchProductId": "1", "price": 20.00, "quantity": 1}]
-        product_json = PhorestApi.order_items_data(self.order.order_items)
-        self.assertEquals(product_json, expected_json)
+        expected_product_json = [{"branchProductId": "1", "price": 10.00, "quantity": 1}, {"branchProductId": "1", "price": 20.00, "quantity": 1}]
+        actual_product_json = PhorestApi.order_items_data(self.order.order_items)
+        self.assertEquals(actual_product_json, expected_product_json)
 
-    def test_should_create_purchase_data(self):
-        expect_purchase_data = {"clientId":"QLbhb0lKCrKmQemqd6h7iA", "items": [{"branchProductId": "1", "price": 10.00, "quantity": 1}, {"branchProductId": "1", "price": 20.00, "quantity": 1}], "number": "11111", "payments": [{"amount": 20.0, "type": "CREDIT"}]}
+    def test_should_create_purchase_json(self):
+        expected_purchase_json = {"clientId":"QLbhb0lKCrKmQemqd6h7iA", "items": [{"branchProductId": "1", "price": 10.00, "quantity": 1}, {"branchProductId": "1", "price": 20.00, "quantity": 1}], "number": "11111", "payments": [{"amount": 20.0, "type": "CREDIT"}]}
 
-        actual_purchase_data = PhorestApi.create_purchase_request_data(self.order.order_items, 20.00, "11111")
-        self.assertEquals(actual_purchase_data, expect_purchase_data)
+        actual_purchase_json = PhorestApi.create_purchase_request_data(self.order.order_items, 20.00, "11111")
+        self.assertEquals(actual_purchase_json, expected_purchase_json)
