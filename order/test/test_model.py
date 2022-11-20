9c7cd60d02daa5f6a2b122e08bae76b35e0fcbae
derekmcauley <derekmcauley7@gmail.com>
2022-11-20 16:22:24 +0000

    fixing Order model test

--- order/test/test_model.py
+++ order/test/test_model.py
@@ -10,11 +10,12 @@ class TestModel(TestCase):
         user = User.objects.create(username = "testuser", password ="userpass1")
         self.order = Order.objects.create(user = user)
         self.order_item = OrderItem.objects.create(order = self.order, price = 10, quantity = 1, product = self.product)
+        self.order.save()
 
     def test_order(self):
         self.assertEquals(self.order.order_items.count(), 1)
         self.assertEquals(self.order.order_items.first(), self.order_item)
-        self.assertEquals(self.order.total_price, 0)
+        self.assertEquals(self.order.total_price, 10)
         self.assertEquals(self.order.order_items.first().product, self.product)
 
 
